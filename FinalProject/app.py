113fa2e7b5e7a8ea0781af825bcec480f957bd48
sibaek <leeseeback1581@gmail.com>
2025-08-28 10:16:42 +0900

    0828 work 2 (refactor)

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-page_2 = st.Page("p2.py", title="개념 학습하기", icon="📖")
-page_3 = st.Page("p3.py", title="학습자료로 질문하기", icon="🙋")
-page_4 = st.Page("p4.py", title="녹음 내용 요약하기", icon="🎙️")
-page_5 = st.Page("p5.py", title="설명문 보기", icon="📄️")
-page_8 = st.Page("p8.py", title="채팅하기", icon="💬")
-page_10 = st.Page("p10.py", title="찍어서 살펴보기", icon="📸")
-page_11 = st.Page("p11.py", title="스터디 플래너", icon="🗓️")
+page_1 = st.Page("page1.py", title="개념 학습하기", icon="📖")
+page_2 = st.Page("page2.py", title="학습자료로 질문하기", icon="🙋")
+page_3 = st.Page("page3.py", title="녹음 내용 요약하기", icon="🎙️")
+page_4 = st.Page("page4.py", title="설명문 보기", icon="📄️")
+page_5 = st.Page("page5.py", title="채팅하기", icon="💬")
+page_6 = st.Page("page6.py", title="찍어서 살펴보기", icon="📸")
+page_7 = st.Page("page7.py", title="스터디 플래너", icon="🗓️")
 
 
-page = st.navigation([page_2, page_3, page_4, page_5, page_8, page_10, page_11])
+page = st.navigation([page_1, page_2, page_3, page_4, page_5, page_6, page_7])
 
 page.run()
