c34a8840d3446efac32942ed621731bcaef0bf72
sibaek <leeseeback1581@gmail.com>
2025-08-28 14:37:49 +0900

    0828 work 5

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
+main_page = st.Page("main.py", title="학습 어시스트", icon="📚")
 page_1 = st.Page("page1.py", title="개념 학습하기", icon="📖")
 page_2 = st.Page("page2.py", title="학습자료로 질문하기", icon="🙋")
 page_3 = st.Page("page3.py", title="녹음 내용 요약하기", icon="🎙️")
@@ -11,6 +13,6 @@ page_8 = st.Page("page8.py", title="달력에 메모하기", icon="📆️")
 
 
 
-page = st.navigation([page_1, page_2, page_3, page_4, page_5, page_6, page_7, page_8])
+page = st.navigation([main_page,page_1, page_2, page_3, page_4, page_5, page_6, page_7, page_8])
 
 page.run()
